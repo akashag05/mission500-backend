29cb72e245fa5ba7bfa75fa9b7c7f300b066a492
karmanshuswami <karmanshu.swami@revdau.com>
2023-09-27 16:09:29 +0530

    added image upload api

--- src/controller/members.controller.ts
+++ src/controller/members.controller.ts
@@ -6,49 +6,49 @@ import CustomRequest from "../middleware/authentication";
 export const addMember = (req: any, res: Response) => {
   try {
     console.log("req file -", req.file);
-    // console.log("req body -", req.body);
-    // const { memberName } = req.body;
-    //   if (!req.file || !memberName) {
-    //     if (req.file) {
-    //       fs.unlinkSync(req.file.path);
-    //     }
-    //     return res.status(400).json({
-    //       type: false,
-    //       error: "Missing required file!",
-    //     });
-    //   } else {
-    //     const fileName = req.file.filename;
-    //     const filePath = `uploads/team/${fileName}`;
-    //     const query =
-    //       "INSERT INTO members (memberName, memberPhoto_path) VALUES (?)";
-    //     const values = [memberName, filePath];
-    //     db.getConnection(function (err, connection) {
-    //       if (err) {
-    //         return res.status(400).json({
-    //           type: false,
-    //           error: err,
-    //           message: "Cannot establish the connection!",
-    //         });
-    //       } else {
-    //         connection.query(query, [values], (err: any, data: any) => {
-    //           if (err) {
-    //             console.log(err);
-    //             return res.json({
-    //               type: false,
-    //               error: err,
-    //               message: "Cannot add the new member!",
-    //             });
-    //           } else {
-    //             return res.status(201).json({
-    //               type: true,
-    //               message: "New member has been added successfully!",
-    //             });
-    //           }
-    //         });
-    //       }
-    //       connection.release();
-    //     });
-    //   }
+    console.log("req body -", req.body);
+    const { memberName } = req.body;
+    console.log(memberName)
+    if (!req.file || !memberName) {
+      if (req.file) {
+        fs.unlinkSync(req.file.path);
+      }
+      return res.status(400).json({
+        type: false,
+        error: "Missing required file!",
+      });
+    } else {
+      const fileName = req.file.filename;
+      const filePath = `uploads/team/${fileName}`;
+      const query = "INSERT INTO members (memberName, memberPhoto_path) VALUES (?)";
+      const values = [memberName, filePath];
+      db.getConnection(function (err, connection) {
+        if (err) {
+          return res.status(400).json({
+            type: false,
+            error: err,
+            message: "Cannot establish the connection!",
+          });
+        } else {
+          connection.query(query, [values], (err: any, data: any) => {
+            if (err) {
+              // console.log(err);
+              return res.json({
+                type: false,
+                error: err,
+                message: "Cannot add the new member!",
+              });
+            } else {
+              return res.status(201).json({
+                type: true,
+                message: "New member has been added successfully!",
+              });
+            }
+          });
+        }
+        connection.release();
+      });
+    }
   } catch (error) {
     return res.status(500).json({
       type: false,
--- src/routes/memberRoutes.ts
+++ src/routes/memberRoutes.ts
@@ -1,13 +1,14 @@
 // Importing all the dependencies here
 import express from "express";
 import { addMember } from "../controller/members.controller";
+import upload from "../middleware/uploadFiles"
 
 // Calling the router function
 const router = express.Router();
 
 // ALl the routes are defined below
 // Main route to test the working of the login_logout route
-router.post("/addMember", addMember);
+router.post("/addMember", upload.single('pdfFile'), addMember);
 
 // This route is used to signup a new user into the database
 // router.post("/signup", authentication, signup);
